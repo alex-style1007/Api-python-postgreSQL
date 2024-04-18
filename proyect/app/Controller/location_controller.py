4bdb84166a3bbcbb9c63741cc57614ea323d9490
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2024-04-18 07:25:15 -0500

    Servicios y controladores de location

---
+++ proyect/app/Controller/location_controller.py
@@ -0,0 +1,25 @@
+# app/controller/location_controller.py
+from service.location_service import LocationService
+
+location_service = LocationService()
+
+def get_all_countries():
+    countries = location_service.get_all_countries()
+    if countries:
+        return countries
+    else:
+        return "Error al obtener los países"
+
+def get_country_by_id(country_id):
+    country = location_service.get_country_by_id(country_id)
+    if country:
+        return country
+    else:
+        return "Error al obtener el país por ID"
+
+def get_reactors_by_country_id(country_id):
+    reactors = location_service.get_reactors_by_country_id(country_id)
+    if reactors:
+        return reactors
+    else:
+        return "Error al obtener los reactores por país"
