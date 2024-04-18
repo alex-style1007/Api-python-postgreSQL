96cfb55952e9d0a2767f2de3601a4396744ebb0e
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2024-04-18 10:01:27 -0500

    controladores y servicios de reactor

--- proyect/app/Controller/reactor_controller.py
+++ proyect/app/Controller/reactor_controller.py
@@ -36,3 +36,37 @@ def get_reactor_by_id(reactor_id):
         return jsonify(reactor.__dict__), 200
     else:
         return {"error": "No se encontró el reactor"}, 404
+
+def get_all_types_reactor():
+    reactor_types = reactor_service.get_all_types_reactor()
+    if reactor_types:
+        return jsonify(reactor_types), 200
+    else:
+        return {"error": "Error al obtener los tipos de reactores"}, 500
+    
+def get_reactors_by_type_id(reactor_id):
+    reactor = reactor_service.get_reactors_by_type_id(reactor_id)
+    if reactor:
+        return jsonify(reactor.__dict__), 200
+    else:
+        return {"error": "No se encontraron reactores asociados a este tipo de reactor"}, 404
+
+def delete_reactor(reactor_id):
+    success = reactor_service.delete_reactor(reactor_id)
+    if success:
+        return {"message": "Reactor eliminado exitosamente"}, 200
+    else:
+        return {"error": "Error al eliminar el reactor"}, 500
+
+def update_reactor(reactor_id, updated_reactor_data):
+    # Obtener el reactor existente por su ID
+    existing_reactor = reactor_service.get_reactor_by_id(reactor_id)
+    if existing_reactor:
+        # Actualizar los datos del reactor existente con los nuevos datos proporcionados
+        for key, value in updated_reactor_data.items():
+            setattr(existing_reactor, key, value)
+        # Llamar al servicio para actualizar el reactor en la base de datos
+        success = reactor_service.update_reactor(existing_reactor)
+        if success:
+            return {"message": "Reactor actualizado exitosamente"}, 200
+    return {"error": "Error al actualizar el reactor"}, 500
--- proyect/app/service/reactor_service.py
+++ proyect/app/service/reactor_service.py
@@ -10,4 +10,17 @@ class ReactorService:
         return self.reactor_repository.create_reactor(reactor)
     
     def get_reactor_by_id(self, reactor_id):
-        return self.reactor_repository.get_reactor_by_id(reactor_id)
+        return self.reactor_repository.reactor_by_id(reactor_id)
+    
+    def get_all_types_reactor(self):
+        return self.reactor_repository.get_all_types_reactor()
+    
+    def get_reactors_by_type_id(self, reactor_id):
+        return self.reactor_repository.get_reactors_by_type_id(reactor_id)
+    
+    def delete_reactor(self, reactor_id):
+        return self.reactor_repository.delete_reactor(reactor_id)
+    
+    def update_reactor(self, reactor):
+        return self.reactor_repository.update_reactor(reactor)
+    
