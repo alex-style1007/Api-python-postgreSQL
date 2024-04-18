793bc5f5e5077073f6662427d6e2df34e32fe0c0
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2024-04-18 06:03:57 -0500

    restructuracion carpeta model

--- proyect/app/model/location.py
+++ proyect/app/model/location.py
@@ -1,4 +1,4 @@
-class Location:
+class Pais:
     def __init__(self, id_pais, nombre_pais):
         self.id_pais = id_pais
         self.nombre_pais = nombre_pais
--- proyect/app/model/type.py
+++
@@ -1,3 +0,0 @@
-class Type:
-    def __init__(self, tipo_reactor):
-        self.tipo_reactor = tipo_reactor
