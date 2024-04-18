cb17fd623e41c920e6f741bbe54b26036ca08d39
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2024-04-17 20:26:00 -0500

    intento del modelo de datos

---
+++ proyect/app/model/location.py
@@ -0,0 +1,4 @@
+class Location:
+    def __init__(self, id_pais, nombre_pais):
+        self.id_pais = id_pais
+        self.nombre_pais = nombre_pais
---
+++ proyect/app/model/type.py
@@ -0,0 +1,3 @@
+class Type:
+    def __init__(self, tipo_reactor):
+        self.tipo_reactor = tipo_reactor
