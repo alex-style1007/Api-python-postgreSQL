32dec497648f0bd0239ec08c3fd6fdb297945bc5
Maragu <maragu.areiza2903@gmail.com>
2024-04-23 07:30:34 -0500

    Otras correcciones

--- proyect/app/controllers/reactor_controller.py
+++ proyect/app/controllers/reactor_controller.py
@@ -1,19 +1,99 @@
 from typing import List
 from fastapi import FastAPI, status, Path, Query, Body, Depends
-from models.reactor_schema import ReactorResponseModel
+from models.reactor_schema import ReactorResponseModel, TypeReactorModel, LocationReactorModel
 from services.reactor_service import ReactorService
 from utils.utils import get_session
 
 
 app = FastAPI()
 
+# 1. Obtener reactores registrados.
 @app.get(
-        path='/reactor',
+        path='/reactors',
         status_code=status.HTTP_200_OK,
         response_model=List[ReactorResponseModel],
 )
-async def get_reactores(session = Depends(get_session)):
+async def get_reactors(session = Depends(get_session)):
     return ReactorService().get_all_reactors()
+
+# 2. Obtener un reactor por Id.
+@app.get(
+        path='/reactors/{id}',
+        status_code=status.HTTP_200_OK,
+        response_model=List[ReactorResponseModel],
+)
+async def get_reactors(session = Depends(get_session), id: int = Path(...)):
+    return ReactorService().get_reactor_by_id(id)
+
+# 3. Crear un nuevo reactor.
+@app.post(
+        path='/reactors',
+        status_code=status.HTTP_200_OK,
+)
+async def get_reactors_by_id(session = Depends(get_session), reactor: ReactorResponseModel=Body(...)):
+    return ReactorService().create_reactor(reactor.model_dump())
+
+# 4. Actualizar un reactor existente.
+@app.put(
+        path='/reactors',
+        status_code=status.HTTP_200_OK,
+)
+async def get_reactors_by_id(session = Depends(get_session), reactor: ReactorResponseModel=Body(...)):
+    return ReactorService().update_reactor(reactor.model_dump())
+
+# 5. Eliminar un reactor existente.
+@app.delete(
+        path='/reactors/{id}',
+        status_code=status.HTTP_200_OK,
+)
+async def get_reactors_by_id(session = Depends(get_session), id: int=Path(...)):
+    return ReactorService().delete_reactor(id)
+
+# 6. Obtener tipos de reactores registrados.
+@app.get(
+        path='/reactors/types',
+        status_code=status.HTTP_200_OK,
+        response_model=List[TypeReactorModel],
+)
+async def get_reactors(session = Depends(get_session)):
+    return ReactorService().get_all_types_reactor()
+
+# 7. Obtener tipo de reactor por Id. Respuesta incluye todos los reactores asociados al tipo.
+@app.get(
+        path='/reactors/types/{id}',
+        status_code=status.HTTP_200_OK,
+        response_model=List[TypeReactorModel],
+)
+async def get_reactors(session = Depends(get_session), id: int = Path(...)):
+    return ReactorService().get_reactors_by_type_id(id)
+
+# 8. Obtener Ubicaciones Registradas.
+@app.get(
+        path='/reactors/locations',
+        status_code=status.HTTP_200_OK,
+        response_model=List[LocationReactorModel],
+)
+async def get_reactors(session = Depends(get_session)):
+    return ReactorService().get_all_locations()
+
+# 9. Obtener Ubicación por Id.
+@app.get(
+        path='/reactors/locations/{id}',
+        status_code=status.HTTP_200_OK,
+        response_model=List[LocationReactorModel],
+)
+async def get_reactors(session = Depends(get_session), id: int = Path(...)):
+    return ReactorService().get_locations_by_id(id)
+
+# 10. Obtener Reactores registrados por Ubicación
+@app.get(
+        path='/reactors/locations/{id}',
+        status_code=status.HTTP_200_OK,
+        response_model=List[LocationReactorModel],
+)
+async def get_reactors(session = Depends(get_session), id: int = Path(...)):
+    return ReactorService().get_locations_by_id(id)
+
 
 
 def create_reactor():
